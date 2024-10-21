f7f07368c0b4e451be6bde87d2fca7059234d4c0
Letícia Magalhães <leticiamassoleni@gmail.com>
2024-10-21 15:19:57 -0300

    movendo as imagens para a pasta assets e implementação do useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import {useState} from "react"
 import './App.scss'
-import lampada from './Components/lampada.png'
+import lampada from './assets/lampada.png'
+import genio_da_lampada from './assets/genio-com-lampada.png'
 
 export default function App(){
+  let imagens = [
+    <img src={lampada} alt="Lâmpada Mágica" />,
+    <img src={genio_da_lampada} alt="Gênio da Lâmpada" />
+  ]
+  const [imagem, setImagem] = useState(imagens[0])
+  const mudarImagem = () => {
+    setImagem(imagens[1])
+  }
   return(
     <>
     <h1>"Liberte a magia da programação com a lâmpada de Aladim"</h1>
-    <img src={lampada} alt="Lâmpada Mágica" />
-    <button>Clique Aqui</button>
+    <div>{imagem}</div>
+    <button onClick={mudarImagem}>Clique Aqui</button>
     </>
   )
 }
