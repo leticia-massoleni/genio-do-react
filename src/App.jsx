a07d38e1abea3972b63476dd19f88e491c6f3a60
Letícia Magalhães <leticiamassoleni@gmail.com>
2024-10-23 15:03:42 -0300

    responsividade e ajustes no html

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import genio_da_lampada from './assets/genio-com-lampada.png'
 
 export default function App(){
   let imagens = [
-    <img src={lampada} alt="Lâmpada Mágica" />,
-    <img src={genio_da_lampada} alt="Gênio da Lâmpada" />
+    <img class='img1' src={lampada} alt="Lâmpada Mágica" />,
+    <img class='img2' src={genio_da_lampada} alt="Gênio da Lâmpada" />
   ]
   const [imagem, setImagem] = useState(imagens[0])
   const mudarImagem = () => {
     setImagem(imagens[1])
   }
   return(
-    <>
-    <h1>"Liberte a magia da programação com a lâmpada de Aladim"</h1>
-    <div>{imagem}</div>
-    <button onClick={mudarImagem}>Clique Aqui</button>
-    </>
+    <main>
+      <h1>"Liberte a magia da programação com a lâmpada de Aladim"</h1>
+      {imagem}
+      <button onClick={mudarImagem}>Clique Aqui</button>
+    </main>
   )
 }
